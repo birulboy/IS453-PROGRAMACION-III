244158355983c77f8526229f51030bcc7860a5da
JuanJoseGalvez <juan.galvez1@utp.edu.co>
2025-02-20 09:43:09 -0500

    Creación de los hechos principales

--- Actividad02/grafo.pl
+++ Actividad02/grafo.pl
@@ -1,1 +1,13 @@
+conexion_directa(vancouver,edmonton,16).
+conexion_directa(vancouver,calgary,13).
 
+conexion_directa(calgary,edmonton,4).
+conexion_directa(calgary,regina,14).
+
+conexion_directa(edmonton,saskatoon,12).
+
+conexion_directa(saskatoon,calgary,9).
+conexion_directa(saskatoon,winnipeg,20).
+
+conexion_directa(regina,saskatoon,7).
+conexion_directa(regina,winnipeg,4).
