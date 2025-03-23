dde1f35a9b71e70e37f322d44dfe9d1eeb673cbb
Brahian Castro Devia <brahian.castro@utp.edu.co>
2025-03-22 19:47:15 -0500

    Create Hermanos and primos

--- Actividad05/Arbol_Simpsons_Listas.pl
+++ Actividad05/Arbol_Simpsons_Listas.pl
@@ -17,9 +17,11 @@ dif(A, B):- A \== B.
 es_abuelo(X,Y):- es_padre(X,Z), (es_padre(Z,Y) ; es_madre(Z,Y)), es_hombre(X).
 es_abuela(X,Y):- es_madre(X,Z), (es_padre(Z,Y) ; es_madre(Z,Y)), es_mujer(X).
 
-
-
+es_hermano(X,Y):- (es_padre(Z,X) ; es_madre(Z,X)) , (es_padre(Z,Y) ; es_madre(Z,Y)) , es_hombre(X) , dif(X,Y).
+es_hermana(X,Y):- (es_padre(Z,X) ; es_madre(Z,X)) , (es_padre(Z,Y) ; es_madre(Z,Y)) , es_mujer(X) , dif(X,Y).
 
 es_tio(X,Y):- es_hermano(X,Z), (es_padre(Z,Y) ; es_madre(Z,Y)).
 es_tia(X,Y):- es_hermana(X,Z), (es_padre(Z,Y) ; es_madre(Z,Y)).
 
+es_primo(X,Y) :- (es_tio(Z,Y) ; es_tia(Z,Y)) , (es_madre(Z,X) ; es_padre(Z,X)) , es_hombre(X).
+es_prima(X,Y) :- (es_tio(Z,Y) ; es_tia(Z,Y)) , (es_madre(Z,X) ; es_padre(Z,X)) , es_mujer(X).
