9597651421cbec58e1596b53a69252dfa3806589
Thomas Santiago Beltran Arias <thomas.beltran@utp.edu.co>
2025-03-22 19:45:30 -0500

    Abuel@s and ti@ addition

--- Actividad05/Arbol_Simpsons_Listas.pl
+++ Actividad05/Arbol_Simpsons_Listas.pl
@@ -12,3 +12,14 @@ es_madre(X,Y):-
     member((X,Y),
     [(marge, bart),(marge, lisa),(marge, maggie),(mona, herbert),(mona, homero),(jacqueline, marge),(jacqueline, patty),(jacqueline, selma),(selma, ling)]).
 
+dif(A, B):- A \== B.
+
+es_abuelo(X,Y):- es_padre(X,Z), (es_padre(Z,Y) ; es_madre(Z,Y)), es_hombre(X).
+es_abuela(X,Y):- es_madre(X,Z), (es_padre(Z,Y) ; es_madre(Z,Y)), es_mujer(X).
+
+
+
+
+es_tio(X,Y):- es_hermano(X,Z), (es_padre(Z,Y) ; es_madre(Z,Y)).
+es_tia(X,Y):- es_hermana(X,Z), (es_padre(Z,Y) ; es_madre(Z,Y)).
+
