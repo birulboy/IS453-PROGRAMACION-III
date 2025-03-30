93eed76f4b47baa1e165a2a8123cf0c9a3427325
juangalvez1 <juan.galvez1@utp.edu.co>
2025-03-29 21:37:47 -0500

    Create basic family operators with structures

---
+++ Actividad06/Arbol_simpsons_estructuras.pl
@@ -0,0 +1,36 @@
+% Definición de personas con su género
+es_hombre(nombre(abraham)). 
+es_hombre(nombre(herbert)). 
+es_hombre(nombre(homero)). 
+es_hombre(nombre(bart)). 
+es_hombre(nombre(clancy)).
+
+es_mujer(nombre(mona)). 
+es_mujer(nombre(marge)). 
+es_mujer(nombre(lisa)). 
+es_mujer(nombre(maggie)). 
+es_mujer(nombre(jacqueline)). 
+es_mujer(nombre(patty)). 
+es_mujer(nombre(selma)). 
+es_mujer(nombre(ling)).
+
+% Relación de padres
+es_padre(nombre(homero), nombre(bart)).
+es_padre(nombre(homero), nombre(lisa)).
+es_padre(nombre(homero), nombre(maggie)).
+es_padre(nombre(abraham), nombre(herbert)).
+es_padre(nombre(abraham), nombre(homero)).
+es_padre(nombre(clancy), nombre(marge)).
+es_padre(nombre(clancy), nombre(patty)).
+es_padre(nombre(clancy), nombre(selma)).
+
+% Relación de madres
+es_madre(nombre(marge), nombre(bart)).
+es_madre(nombre(marge), nombre(lisa)).
+es_madre(nombre(marge), nombre(maggie)).
+es_madre(nombre(selma), nombre(ling)).
+es_madre(nombre(mona), nombre(herbert)).
+es_madre(nombre(mona), nombre(homero)).
+es_madre(nombre(jacqueline), nombre(marge)).
+es_madre(nombre(jacqueline), nombre(patty)).
+es_madre(nombre(jacqueline), nombre(selma)).
