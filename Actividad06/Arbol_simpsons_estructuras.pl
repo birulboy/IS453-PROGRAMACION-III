99853986797d74e9ec4a742b1a51c6835cbaafc0
juangalvez1 <juan.galvez1@utp.edu.co>
2025-04-01 23:16:49 -0500

    Changing excersice to structures and list, basic family operators

--- Actividad06/Arbol_simpsons_estructuras.pl
+++ Actividad06/Arbol_simpsons_estructuras.pl
@@ -1,55 +1,20 @@
-% Definición de personas con su género
-es_hombre(nombre(abraham)). 
-es_hombre(nombre(herbert)). 
-es_hombre(nombre(homero)). 
-es_hombre(nombre(bart)). 
-es_hombre(nombre(clancy)).
+es_hombre(X):- 
+    member(X, [hombre(abraham), hombre(herbert), hombre(homero), hombre(bart), hombre(clancy)]).
 
-es_mujer(nombre(mona)). 
-es_mujer(nombre(marge)). 
-es_mujer(nombre(lisa)). 
-es_mujer(nombre(maggie)). 
-es_mujer(nombre(jacqueline)). 
-es_mujer(nombre(patty)). 
-es_mujer(nombre(selma)). 
-es_mujer(nombre(ling)).
+es_mujer(X):- 
+    member(X, [mujer(mona), mujer(marge), mujer(lisa), mujer(maggie), mujer(jacqueline), mujer(patty), mujer(selma), mujer(ling)]).
 
-% Relación de padres
-es_padre(nombre(homero), nombre(bart)).
-es_padre(nombre(homero), nombre(lisa)).
-es_padre(nombre(homero), nombre(maggie)).
-es_padre(nombre(abraham), nombre(herbert)).
-es_padre(nombre(abraham), nombre(homero)).
-es_padre(nombre(clancy), nombre(marge)).
-es_padre(nombre(clancy), nombre(patty)).
-es_padre(nombre(clancy), nombre(selma)).
+es_padre(X,Y):- 
+    member((X,Y),
+    [(hombre(homero), hombre(bart)), (hombre(homero), mujer(lisa)), (hombre(homero), mujer(maggie)), 
+     (hombre(abraham), hombre(herbert)), (hombre(abraham), hombre(homero)), 
+     (hombre(clancy), mujer(marge)), (hombre(clancy), mujer(patty)), (hombre(clancy), mujer(selma))]).
 
-% Relación de madres
-es_madre(nombre(marge), nombre(bart)).
-es_madre(nombre(marge), nombre(lisa)).
-es_madre(nombre(marge), nombre(maggie)).
-es_madre(nombre(selma), nombre(ling)).
-es_madre(nombre(mona), nombre(herbert)).
-es_madre(nombre(mona), nombre(homero)).
-es_madre(nombre(jacqueline), nombre(marge)).
-es_madre(nombre(jacqueline), nombre(patty)).
-es_madre(nombre(jacqueline), nombre(selma)).
+es_madre(X,Y):- 
+    member((X,Y),
+    [(mujer(marge), hombre(bart)), (mujer(marge), mujer(lisa)), (mujer(marge), mujer(maggie)), 
+     (mujer(mona), hombre(herbert)), (mujer(mona), hombre(homero)), 
+     (mujer(jacqueline), mujer(marge)), (mujer(jacqueline), mujer(patty)), (mujer(jacqueline), mujer(selma)), 
+     (mujer(selma), mujer(ling))]).
 
 dif(A, B):- A \== B.
-
-% Relación de abuelos
-es_abuelo(nombre(X), nombre(Z)) :- es_padre(nombre(X), nombre(Y)), (es_padre(nombre(Y), nombre(Z)) ; es_madre(nombre(Y), nombre(Z))).
-es_abuela(nombre(X), nombre(Z)) :- es_madre(nombre(X), nombre(Y)), (es_padre(nombre(Y), nombre(Z)) ; es_madre(nombre(Y), nombre(Z))).
-
-% Relación de hermanos
-es_hermano(nombre(X), nombre(Y)) :- 
-    es_hombre(nombre(X)),
-    X @< Y,  % Impide que Prolog repita la relación en orden inverso
-    (es_padre(nombre(Z), nombre(X)), es_padre(nombre(Z), nombre(Y)));
-    (es_madre(nombre(Z), nombre(X)), es_madre(nombre(Z), nombre(Y))), es_hombre(nombre(X)), dif(X,Y).
-
-es_hermana(nombre(X), nombre(Y)) :- 
-    es_mujer(nombre(X)),
-    X @< Y,  % Impide que Prolog repita la relación en orden inverso
-    (es_padre(nombre(Z), nombre(X)), es_padre(nombre(Z), nombre(Y)));
-    (es_madre(nombre(Z), nombre(X)), es_madre(nombre(Z), nombre(Y))), es_mujer(nombre(X)), dif(X,Y).
